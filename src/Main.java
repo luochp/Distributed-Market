255daed464d49c5faffac152eb0a3dc2c1595feb
chuanpuluo <luochuanpu@gmail.com>
2020-02-25 11:28:09 -0500

    create class and interface

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class setup{
+    public static void main(String[] args){
+
+        // Read configuration file
+
+        // Create Nodes and Run Nodes
+        pass;
+
+    }
+}
